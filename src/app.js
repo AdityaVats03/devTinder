633f3ccbd70e6075d430e1d18ada0a5abcffd076
Aditya <aditya@Adityas-MacBook-Air.local>
2025-09-17 13:54:29 +0530

    testing routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,29 @@
 const express = require("express");
 const app = express();
 
+// app.get("/user", (req, res) => {
+//   res.send({ firstName: "Aditya", lastname: "Sharma" });
+// });
+
+app.get("/user", (req, res) => {
+  console.log(req.query);
+  res.send({ firstName: "Aditya", lastname: "Sharma" });
+});
+
+app.get("/user/:userId", (req, res) => {
+  console.log(req.params);
+  res.send({ firstName: "Aditya", lastname: "Sharma" });
+});
+
+app.post("/user", (req, res) => {
+  console.log(req.body);
+  res.send("Data successfully saved to database");
+});
+
+app.use("/test", (req, res) => {
+  res.send("Hello from test Server");
+});
+
 app.use((req, res) => {
   res.send("Hello from Server");
 });
