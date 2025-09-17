f802b80b89dd56707ca368b3da7312871611e8a6
Aditya <aditya@Adityas-MacBook-Air.local>
2025-09-17 12:42:55 +0530

    created server

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.use((req, res) => {
+  res.send("Hello from Server");
+});
+
+app.listen(3000, () => {
+  console.log("Server is successfuly listening on port 3000 with nodemon...");
+});
